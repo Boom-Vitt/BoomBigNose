43a2ff4706b072c5f46307a9dd008836a88f879a
Vittawat <vittawat.soo@gmail.com>
2025-05-23 16:37:20 +0700

    Simplified deployment to remove all Docker dependencies

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
-set -e
-
+#!/bin/sh
 echo "Starting BoomBigNose Template Landing Page"
-
-# Start nginx
-echo "Starting nginx..."
-nginx
-
-# Keep the container running
 echo "Server is running at http://localhost:8000"
-tail -f /var/log/nginx/access.log
+nginx -g "daemon off;"
